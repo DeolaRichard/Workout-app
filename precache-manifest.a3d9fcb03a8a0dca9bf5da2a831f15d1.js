07ecc0df24e49754dce8cadae4d3275c88dfa101
DeolaRichard <adekunlerichie5@gmail.com>
2023-06-02 03:14:28 +0100

    Deploy Workout React app to GitHub Pages

---
+++ precache-manifest.a3d9fcb03a8a0dca9bf5da2a831f15d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d4a90189122d66af048",
+    "url": "/%7BWorkout-app%7D/static/css/main.ba35061c.chunk.css"
+  },
+  {
+    "revision": "7d4a90189122d66af048",
+    "url": "/%7BWorkout-app%7D/static/js/main.09e3bca6.chunk.js"
+  },
+  {
+    "revision": "147418b6cfade2993b00",
+    "url": "/%7BWorkout-app%7D/static/js/runtime~main.d796f03a.js"
+  },
+  {
+    "revision": "bc8216ec2217cbea6df6",
+    "url": "/%7BWorkout-app%7D/static/js/2.4978d42e.chunk.js"
+  },
+  {
+    "revision": "61643d51a5b2e2abeb875a13c7a29eda",
+    "url": "/%7BWorkout-app%7D/index.html"
+  }
+];
